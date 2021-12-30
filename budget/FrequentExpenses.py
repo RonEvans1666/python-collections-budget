0ee5bdd10ba6705af135c642d907cd792a468868
RonEvans1666 <91101086+RonEvans1666@users.noreply.github.com>
2021-12-30 12:59:19 -0500

    expenses

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,3 +1,4 @@
 from .import Expense
-expenses = Expenses.Expenses()
-expenses.read_expenses('data/spending_data.csv')
+
+ expenses = Expenses.Expenses()
+ expenses.read_expenses('data/spending_data.csv')
